2af2b70bf64f7ca61def40dece90b03b0d1fbeb0
Nadav <nadavoffice@gmail.com>
2014-07-01 10:17:47 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unityone::Application.config.secret_key_base = 'f17b8e47d7bdf2d0e7bd4562e0b070cdc5d3c9494c7295d3ffe3000262ebf94768eba5a0de7409d49b41d72fcae7ae4af3d4496651ebb516c96da484c0eac7d8'
